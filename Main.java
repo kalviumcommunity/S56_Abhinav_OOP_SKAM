5d4f725713c1f55ae28c7a9eac452285d70d96ba
Abhinav <144326505+abhinav0306@users.noreply.github.com>
2024-09-14 18:43:36 +0530

    Encapsulation concept added.

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 class User {
+    // Private attributes
     private String id;
     private String name;
     private String email;
@@ -12,35 +13,51 @@ class User {
         userCount++;
     }
 
+    // Getter methods
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void displayUserInfo() {
-        System.out.println("User ID: " + this.id);
-        System.out.println("User Name: " + this.name);
-        System.out.println("User Email: " + this.email);
+        System.out.println("User ID: " + getId());
+        System.out.println("User Name: " + getName());
+        System.out.println("User Email: " + getEmail());
         System.out.println();
     }
 
-    public void updateName(String newName) {
-        this.name = newName;
-    }
-
-    public void updateEmail(String newEmail) {
-        this.email = newEmail;
-    }
     public static int getUserCount() {
         return userCount;
     }
-    public static String getOrganization(){
+
+    public static String getOrganization() {
         return organization;
     }
 }
 
 class Assignment {
+    // Private attributes
     private String id;
     private String title;
     private String description;
     private String dueDate;
     private static int assignmentCount = 0;
-
 
     public Assignment(String id, String title, String description, String dueDate) {
         this.id = id;
@@ -50,17 +67,34 @@ class Assignment {
         assignmentCount++;
     }
 
-    public void displayAssignmentInfo() {
-        System.out.println("Assignment ID: " + this.id);
-        System.out.println("Assignment Title: " + this.title);
-        System.out.println("Assignment Description: " + this.description);
-        System.out.println("Assignment Due Date: " + this.dueDate);
-
-        System.out.println();
+    // Getter methods
+    public String getId() {
+        return id;
     }
 
-    public void updateDueDate(String newDueDate) {
-        this.dueDate = newDueDate;
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    // Setter methods
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void displayAssignmentInfo() {
+        System.out.println("Assignment ID: " + getId());
+        System.out.println("Assignment Title: " + getTitle());
+        System.out.println("Assignment Description: " + getDescription());
+        System.out.println("Assignment Due Date: " + getDueDate());
+        System.out.println();
     }
 
     public static int getAssignmentCount() {
@@ -72,11 +106,11 @@ public class Main {
     public static void main(String[] args) {
         User[] users = {
                 new User("S101", "Abhinav", "abhinav.singh@kalvium.community"),
-                new User("S102","Ayush Ghodke","ayush.ghodke@kalvium.community"),
-                new User("S103","Shriyans Jindal","shriyans.jindal@kalvium.community"),
-                new User("S104","Dhruv Patil","dhruv.patil@kalvium.community"),
+                new User("S102", "Ayush Ghodke", "ayush.ghodke@kalvium.community"),
+                new User("S103", "Shriyans Jindal", "shriyans.jindal@kalvium.community"),
+                new User("S104", "Dhruv Patil", "dhruv.patil@kalvium.community"),
                 new User("M101", "Manu", "manu.a@kalvium.community"),
-                new User("M102","Deepakshi","deepakshi.sharma@kalvium.community")
+                new User("M102", "Deepakshi", "deepakshi.sharma@kalvium.community")
         };
 
         Assignment[] assignments = {
@@ -89,21 +123,19 @@ public class Main {
             user.displayUserInfo();
         }
 
-        users[0].updateName("Abhishek");
-        users[0].updateEmail("abhishek@kalvium.community");
+        users[0].setName("Abhishek");
+        users[0].setEmail("abhishek@kalvium.community");
         users[0].displayUserInfo();
 
         for (Assignment assignment : assignments) {
             assignment.displayAssignmentInfo();
         }
 
-        assignments[0].updateDueDate("02-09-2024");
+        assignments[0].setDueDate("02-09-2024");
         assignments[0].displayAssignmentInfo();
 
-//        JAVA DOESN'T SUPPORT DELETE KEYWORD
-        System.out.println("Organization: "+ User.getOrganization());
+        System.out.println("Organization: " + User.getOrganization());
         System.out.println("Total Users: " + User.getUserCount());
         System.out.println("Total Assignments: " + Assignment.getAssignmentCount());
-
     }
 }
